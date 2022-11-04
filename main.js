66b0db510f8018ab78de0c25f2a39bd00dcfd81f
Sebastian Alejandro Maria <sebastianalejandromaria@gmail.com>
2022-11-04 18:00:43 -0300

    Agregamos funciones para renderizar al jugador y para poder moverlo dentro del canvas

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Variables globales //
+
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 let elementSize;
@@ -6,7 +8,12 @@ const upButton = document.querySelector('#up');
 const leftButton = document.querySelector('#left');
 const rigthButton = document.querySelector('#rigth');
 const downButton = document.querySelector('#down');
+const playerPosition = {
+  x : undefined,
+  y : undefined,
+};
 
+// EventListeners //
 
 upButton.addEventListener('click', movePlayerUp),
 leftButton.addEventListener('click', movePlayerLeft),
@@ -15,6 +22,8 @@ downButton.addEventListener('click', movePlayerDown),
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
 window.addEventListener('keydown', moveByKeys);
+
+// Funciones //
 
 function setCanvasSize (){
     if (window.innerHeight > window.innerWidth) {
@@ -35,32 +44,63 @@ function startGame () {
     game.font = elementSize + 'px Verdana';
     game.textAlign = 'end';
 
-    let map = maps[1];
+    let map = maps[0];
     const mapRows = map.trim().split('\n');
     const mapRowsCols = mapRows.map(row => row.trim().split(''));
 
+    game.clearRect(0,0,canvasSize,canvasSize);
     mapRowsCols.forEach((row, rowI ) => {
         row.forEach((col, colI) => {
             const emoji = emojis[col];
-            const posX = elementSize * (colI + 1) + 5;
-            const posY = elementSize * (rowI + 1) - 8;
+            const posX = elementSize * (colI + 1);
+            const posY = elementSize * (rowI + 1);
+
+            if (col === 'O'){
+              if (!playerPosition.x && !playerPosition.y){
+                playerPosition.x = posX;
+                playerPosition.y = posY;
+              }
+            } 
+
             game.fillText(emoji, posX, posY);
         });
     });
+
+    movePlayer();
+}
+
+function movePlayer(){
+  game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
 }
 
 function movePlayerUp(){
-    console.log('te fuiste parriba')
+    console.log('te fuiste parriba');
+  if ((playerPosition.y - elementSize) < elementSize) {
+    console.log('OUT');
+  } else {
+    playerPosition.y -= elementSize;
+    startGame();
+  }
 };
+
 function movePlayerLeft(){
-    console.log('te fuiste palaizquierda')
+    console.log('te fuiste palaizquierda');
+    playerPosition.x -= elementSize;
+    startGame();
 };
+
 function movePlayerRight(){
-    console.log('te fuiste paladerecha')
+    console.log('te fuiste paladerecha');
+    playerPosition.x += elementSize;
+    startGame();
 };
+
 function movePlayerDown(){
-    console.log('te fuiste pabajo')
+    console.log('te fuiste pabajo');
+    playerPosition.y += elementSize;
+    startGame();
 };
+
 function moveByKeys(event){
   if (event.key == 'ArrowUp'){
     movePlayerUp();
